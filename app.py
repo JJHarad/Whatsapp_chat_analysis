6ced90bb1b1c415406cc4fdd731e6492403824d6
JJHarad <jidnyaharad11@gmail.com>
2024-09-06 03:15:32 +0530

    Find words in messages

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 import streamlit as st
-import preprocessor,helper
-st.sidebar.title("Whatsapp chat analyser")
+import preprocessor
+import helper
+
+# Title of the Streamlit app
+st.sidebar.title("WhatsApp Chat Analyzer")
+
+# File uploader widget
 uploaded_file = st.sidebar.file_uploader("Choose a file")
+
 if uploaded_file is not None:
+    # Read the file and preprocess
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
     df = preprocessor.preprocess(data)
+
+    # Display the dataframe
     st.dataframe(df)
 
-    # find unique user
+    # Find unique users
     user_list = df['users'].unique().tolist()
     user_list.remove('group_notification')
     user_list.sort()
-    user_list.insert(0,"Overall")
-    selected_user = st.sidebar.selectbox("Show Analysis wrt", user_list)
+    user_list.insert(0, "Overall")
+    selected_user = st.sidebar.selectbox("Show Analysis with respect to", user_list)
 
-    #create button
-    if st.sidebar.button("show analysis"):
-        num_messages = helper.fetch_stats(selected_user,df)
+    # Create button to show analysis
+    if st.sidebar.button("Show Analysis"):
+        num_messages, num_words = helper.fetch_stats(selected_user, df)
 
-        col1, col2, col3, col4 = st.columns(4)
+        # Display the results in columns
+        col1, col2 = st.columns(2)
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(num_words)
